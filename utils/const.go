51d79e1d6bcffab825c1240b4a6a39d6fc2cac30
Vaibhav <vrongmeal@gmail.com>
2018-12-13 01:26:04 +0530

    complete utils with rubeus files constants

---
+++ utils/const.go
@@ -0,0 +1,132 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func getHomePath() string {
+	osys := runtime.GOOS
+	if osys == "windows" {
+		return os.Getenv("HOMEPATH")
+	}
+	return os.Getenv("HOME")
+}
+
+// BroConfPath is the path of config file storing Gamepath
+var BroConfPath = filepath.Join(getHomePath(), ".brocli")
+
+// Constants
+const (
+	NLn = "\n      " // New line with indentation
+
+	UserInitH = `#pragma once
+
+// Include Game levels here as
+// #include "game_name/engine_files/level.level_name.h"
+`
+	UserInitCpp = `#include "user_init.h"
+#include "../RubeusCore.h"
+
+// Initialise levels here as
+// SampleLevel * sample_level = new SampleLevel("sample_level");
+
+// Add startup level as
+// std::string startupLevel = "sample_level";
+
+// Initialise game objects as
+// SampleObject * sample_object = new SampleObject(
+// 	   "sample_object",
+// 	   "sample_level",
+// 	   6.0f, 3.0f,
+// 	   3.0f, 3.0f,
+// 	   "Assets/debug.png",
+// 	   false
+// );
+`
+	LevelH = `#pragma once
+
+#include "../RubeusCore.h"
+
+class %s : public Rubeus::RLevel
+{
+protected:
+	// User defined members
+
+
+	// Don't look down
+public:
+	%s(std::string name)
+		: RLevel(name)
+	{
+	}
+
+	~%s()
+	{
+	}
+
+	void begin() override;
+	void onEnd() override;
+};
+`
+	LevelCpp = `#include "%s.h"
+
+void %s::begin()
+{
+}
+
+void %s::onEnd()
+{
+}
+`
+	ObjectH = `#pragma once
+
+#include "../RubeusCore.h"
+
+class %s : public Rubeus::RGameObject
+{
+	// User members
+
+
+
+	// Do not look below
+public:
+
+	%s(std::string name, std::string levelName, float x, float y, float deltaX, float deltaY, const char * imageFilePath, bool enablePhysics = false, const Rubeus::Awerere::EColliderType & type = Rubeus::Awerere::EColliderType::NO_COLLIDER, Rubeus::Awerere::ACollider * collider = NULL, bool generatesHit = false, const Rubeus::Awerere::APhysicsMaterial & physicsMat = Rubeus::Awerere::APhysicsMaterial())
+		: RGameObject(name, levelName, x, y, deltaX, deltaY, imageFilePath, enablePhysics, type, collider, generatesHit, physicsMat)
+	{
+	}
+
+	// TODO: Add default paramters
+	%s(std::string name, std::string levelName, float x, float y, float deltaX, float deltaY, float & r, float & g, float & b, bool enablePhysics = false, const Rubeus::Awerere::APhysicsMaterial & material = Rubeus::Awerere::APhysicsMaterial(), const Rubeus::Awerere::EColliderType & type = Rubeus::Awerere::EColliderType::NO_COLLIDER, Rubeus::Awerere::ACollider * collider = NULL, bool generatesHit = false)
+		: RGameObject(name, levelName, x, y, deltaX, deltaY, r, g, b, enablePhysics, material, type, collider, generatesHit)
+	{
+	}
+
+	void begin() override;
+	void onHit(RGameObject * hammer, RGameObject * nail, const Rubeus::Awerere::ACollideData & collisionData) override;
+	void onMessage(Rubeus::Message * msg) override;
+	void tick() override;
+};
+`
+
+	ObjectCpp = `#include "%s.h"
+
+void %s::begin()
+{
+}
+
+void %s::onHit(RGameObject * hammer, RGameObject * nail, const Rubeus::Awerere::ACollideData & collisionData)
+{
+}
+
+void %s::tick()
+{
+}
+
+void %s::onMessage(Rubeus::Message * msg)
+{
+}
+`
+)
--- utils/utils.go
+++ utils/utils.go
@@ -3,22 +3,7 @@ package utils
 import (
 	"os"
 	"path"
-	"path/filepath"
-	"runtime"
-)
-
-func getHomePath() string {
-	osys := runtime.GOOS
-	if osys == "windows" {
-		return os.Getenv("HOMEPATH")
-	}
-	return os.Getenv("HOME")
-}
-
-// Constants
-var (
-	NLn         = "\n      " // New line with indentation
-	BroConfPath = filepath.Join(getHomePath(), ".brocli")
+	"regexp"
 )
 
 // DoesExist checks if the given path exists
@@ -42,4 +27,29 @@ func IsDir(pth string) bool {
 		return true
 	}
 	return false
+}
+
+// IsFile checks if the path is a regular file
+func IsFile(pth string) bool {
+	// Assuming it exists
+	stat, _ := os.Stat(pth)
+	mode := stat.Mode()
+	if !mode.IsDir() && mode.IsRegular() {
+		return true
+	}
+	return false
+}
+
+// IsValidVarName checks if the given string can be accepted as a variable name
+func IsValidVarName(s string) bool {
+	// A valid variable name can start with either an alpha character or an underscore
+	// Other caracters can be alphanumeric or underscore
+	regex := regexp.MustCompile(`^[A-Za-z_][0-9A-Za-z_]*$`)
+	return regex.MatchString(s)
+}
+
+// IsValidName checks if the given string includes only alphanumeric and underscore
+func IsValidName(s string) bool {
+	regex := regexp.MustCompile(`^[0-9A-Za-z_]*$`)
+	return regex.MatchString(s)
 }
